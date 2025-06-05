99a82c01fcf6ca9aeb64d918247fbba31bcbe976
marcelo.rispoli.leal <marcelorispoli@gmail.com>
2025-06-05 05:30:34 -0300

    Adjusted locales and imported images for future slider

---
+++ src/components/Slider.jsx
@@ -0,0 +1,18 @@
+import {
+  IoIdCardOutline,
+  IoCodeSlash,
+  IoLogoAmazon,
+  IoConstructOutline,
+  IoServerOutline,
+  IoDesktopOutline,
+} from "react-icons/io5";
+
+// Mapping icon names to icon components
+const iconMap = {
+  IoIdCardOutline,
+  IoCodeSlash,
+  IoLogoAmazon,
+  IoConstructOutline,
+  IoServerOutline,
+  IoDesktopOutline,
+};
