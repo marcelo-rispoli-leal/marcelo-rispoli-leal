fbb4e565d2282161fc7f18af6a7a42ade2ed66bf
marcelo.rispoli.leal <marcelorispoli@gmail.com>
2025-06-10 14:39:13 -0300

    json and carrousel adjustment

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -42,19 +42,18 @@ export default function Carousel() {
   const IconComponent = iconMap[currentSlide.icon];
 
   return (
-    <section className="rounded-3xl bg-teal-100/80 py-8 dark:bg-teal-900/20">
-      <div className="px-6">
-        <h2 className="mb-12 text-2xl text-teal-700 underline md:text-3xl xl:text-4xl dark:text-teal-300">
-          <IconComponent className="mr-1" />
-          {title}
-        </h2>
-
+    <section className="rounded-3xl bg-teal-100/80 py-6 md:py-16 dark:bg-teal-900/20">
+      <h2 className="mb-4 text-xl text-teal-700 underline md:mb-12 md:text-2xl xl:text-3xl dark:text-teal-300">
+        <IconComponent className="mr-1" />
+        {title}
+      </h2>
+      <div className="px-4 md:px-8">
         <div className="relative">
           {/* Container with fixed aspect ratio */}
-          <div className="w-full pb-[30%]">
+          <div className="w-full pb-[80%] md:pb-[30%]">
             {/* Grid for images */}
             <div
-              className={`absolute inset-0 grid gap-8 px-4`}
+              className={`absolute inset-0 grid gap-3 px-2 md:gap-8 md:px-4`}
               style={{
                 gridTemplateColumns: `repeat(${currentSlide.columns}, minmax(0, 1fr))`,
                 gridTemplateRows: `repeat(${currentSlide.rows}, minmax(0, 1fr))`,
@@ -83,28 +82,28 @@ export default function Carousel() {
           {/* Navegation buttons */}
           <button
             onClick={prevSlide}
-            className="absolute top-1/2 left-0 -translate-y-1/2 rounded-full bg-neutral-500 p-2 text-neutral-50 hover:bg-neutral-600 md:-left-12 dark:hover:bg-neutral-400"
+            className="absolute top-1/2 -left-8 -translate-y-1/2 rounded-full bg-neutral-500 p-2 text-neutral-50 hover:bg-neutral-600 md:-left-[56px] dark:hover:bg-neutral-400"
             aria-label="Previous slide"
           >
-            <IoChevronBackOutline className="h-6 w-6 md:h-8 md:w-8" />
+            <IoChevronBackOutline className="h-4 w-4 md:h-8 md:w-8" />
           </button>
           <button
             onClick={nextSlide}
-            className="absolute top-1/2 right-0 -translate-y-1/2 rounded-full bg-neutral-500 p-2 text-neutral-50 hover:bg-neutral-600 md:-right-12 dark:hover:bg-neutral-400"
+            className="absolute top-1/2 -right-8 -translate-y-1/2 rounded-full bg-neutral-500 p-2 text-neutral-50 hover:bg-neutral-600 md:-right-[56px] dark:hover:bg-neutral-400"
             aria-label="Next slide"
           >
-            <IoChevronForwardOutline className="h-6 w-6 md:h-8 md:w-8" />
+            <IoChevronForwardOutline className="h-4 w-4 md:h-8 md:w-8" />
           </button>
         </div>
 
         {/* Slides indicators (dots) */}
-        <div className="mt-8 flex flex-col items-center justify-center space-y-4">
+        <div className="mt-6 flex flex-col items-center justify-center space-y-4 md:mt-8">
           <div className="flex justify-center space-x-2">
             {slides.map((_, index) => (
               <button
                 key={index}
                 onClick={() => setCurrentSlideIndex(index)}
-                className={`h-3 w-3 rounded-full transition-colors ${
+                className={`h-2 w-2 rounded-full transition-colors md:h-3 md:w-3 ${
                   index === currentSlideIndex
                     ? "bg-teal-700 dark:bg-teal-300"
                     : "bg-neutral-500 hover:bg-neutral-600 dark:hover:bg-neutral-400"
@@ -119,9 +118,9 @@ export default function Carousel() {
             aria-label={isPlaying ? "Pause slide show" : "Play slide show"}
           >
             {isPlaying ? (
-              <IoPause className="h-6 w-6 md:h-8 md:w-8" />
+              <IoPause className="h-4 w-4 md:h-8 md:w-8" />
             ) : (
-              <IoPlay className="h-6 w-6 md:h-8 md:w-8" />
+              <IoPlay className="h-4 w-4 md:h-8 md:w-8" />
             )}
           </button>
         </div>
